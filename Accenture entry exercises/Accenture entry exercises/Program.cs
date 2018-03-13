8f88a5289adebaf41fe1bc87ed78bba97948357a
Fellner Sára <Sara.Fellner@loxon.eu>
2018-03-13 17:45:28 +0100

    1st exercise

---
+++ Accenture entry exercises/Accenture entry exercises/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accenture_entry_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        
+    }
+}
