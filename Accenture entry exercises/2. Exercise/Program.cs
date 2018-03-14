d98e612f65379a5fee5279b0091c10ad21e380f3
Fellner Sára <Sara.Fellner@loxon.eu>
2018-03-14 17:51:54 +0100

    2nd Exercise

---
+++ Accenture entry exercises/2. Exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static string Encode(string text)
+        {
+            string encodedText = "";
+
+
+
+            return encodedText;
+
+        }
+    }
+}
