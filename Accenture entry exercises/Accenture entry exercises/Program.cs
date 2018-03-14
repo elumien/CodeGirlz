35b682242b76586a707efc8194d1d047cd91aa01
Fellner Sára <Sara.Fellner@loxon.eu>
2018-03-14 17:42:04 +0100

    1st exercise

--- Accenture entry exercises/Accenture entry exercises/Program.cs
+++ Accenture entry exercises/Accenture entry exercises/Program.cs
@@ -10,8 +10,48 @@ namespace Accenture_entry_exercises
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Input: [1, 2, 3, 4, 5, 6]");
+
+            int[] example1 = { 1, 2, 3, 4, 5, 6 };
+
+            double average1 = OddAverage(example1);
+
+            Console.WriteLine("Output: " + average1);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Input: [-3, -2, -1, 0, 1, 2, 3, 5, 7]");
+
+            int[] example2 = {-3, -2, -1, 0, 1, 2, 3, 5, 7};
+
+            //int[] example2 = {1, 13, 7, 5, 32523};
+
+            double average2 = OddAverage(example2);
+
+            Console.WriteLine("Output: " + average2);
+
+            Console.Read();
         }
 
-        
+        public static double OddAverage(int[] numbers)
+        {
+            double dividend = 0;
+            double average = 0;
+            double divisor = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 1 || number % 2 == - 1)
+                {
+                    dividend += number;
+                    divisor++;
+                }
+            }
+
+            average = dividend / divisor;
+
+            return average;
+            
+        }
     }
 }
