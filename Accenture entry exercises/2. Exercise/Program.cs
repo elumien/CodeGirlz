8009a3437c8053308a089fcd49dd6cc10098e734
elumien <sara.fellner@gmail.com>
2018-03-15 09:01:43 +0100

    2nd exercise

--- Accenture entry exercises/2. Exercise/Program.cs
+++ Accenture entry exercises/2. Exercise/Program.cs
@@ -10,16 +10,50 @@ namespace _2.Exercise
     {
         static void Main(string[] args)
         {
+			Encode("apple");
+			Encode("apple alarm");
+			Encode("zoo");
+			Console.Read();
 
-        }
+		}
 
-        public static string Encode(string text)
+		public static string Encode(string text)
         {
-            string encodedText = "";
+			string encodedText = "";
+			string abc = "abcdefghijklmnopqrstuvwxyz";
 
+			text = text.ToLower();
 
+			char[] characters = text.ToCharArray();
 
-            return encodedText;
+			int shift = text.Count(c => c == characters[0]);
+
+			foreach (char character in characters)
+			{
+				int index = abc.IndexOf(character);
+
+				if (index == -1)
+				{
+					encodedText += character;
+				}
+				else
+				{
+					int encodedIndex = index + shift;
+
+					if (encodedIndex > 25)
+					{
+						encodedIndex -= 26;
+					}
+					encodedText += abc[encodedIndex];
+				}
+
+			}
+
+			Console.WriteLine("Input: " + text);
+			Console.WriteLine("Output: " + encodedText);
+			Console.WriteLine();
+
+			return encodedText;
 
         }
     }
